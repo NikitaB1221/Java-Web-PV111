c2b73482468657e0d777d3eeb8fcf9d74722c09d
Bodu_wk69 <Bodu_wk69@student.itstep.org>
2024-03-05 17:19:05 +0200

    EncodingFilter

---
+++ src/main/java/step/learning/filters/EncodingFilter.java
@@ -0,0 +1,42 @@
+package step.learning.filters;
+
+import com.google.inject.Singleton;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Singleton
+public class EncodingFilter implements Filter {
+
+    private FilterConfig filterConfig;
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        String path = httpRequest.getRequestURI().substring(httpRequest.getContextPath().length());
+
+        if (path.startsWith("/css/") || path.startsWith("/js/") || path.startsWith("/upload/") || path.startsWith("/img/")) {
+            // Пропустить фильтрацию для этих путей
+            chain.doFilter(request, response);
+        } else {
+            // Установить кодировку символов для всех остальных запросов и ответов
+            httpRequest.setCharacterEncoding("UTF-8");
+            httpResponse.setCharacterEncoding("UTF-8");
+            chain.doFilter(httpRequest, httpResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        this.filterConfig = null;
+    }
+}
--- src/main/java/step/learning/ioc/RouterModule.java
+++ src/main/java/step/learning/ioc/RouterModule.java
@@ -3,12 +3,14 @@ package step.learning.ioc;
 import com.google.inject.servlet.ServletModule;
 import step.learning.filters.AuthFilter;
 import step.learning.filters.DbFilter;
+import step.learning.filters.EncodingFilter;
 import step.learning.servlets.*;
 
 public class RouterModule extends ServletModule {
     @Override
     protected void configureServlets() {
-        filterRegex("^/(?!css/.+|js/.+|img/.+).*$").through(DbFilter.class);
+        filterRegex("^/(?!css/.+|js/.+|img/.+|upload/.+|resources/.+).*$").through(EncodingFilter.class);
+        filterRegex("^/(?!css/.+|js/.+|img/.+|resources/.+).*$").through(DbFilter.class);
         filter("/*").through(AuthFilter.class);
 
         serve("/").with(HomeServlet.class);
